58b33eba6936863108fa394167b7097f0513f902
Alex <arodtony89@gmail.com>
2020-08-15 23:09:47 -0400

    added Amplify information

--- App.js
+++ App.js
@@ -3,11 +3,10 @@ import {InfoProvider} from 'USC_app_v1/Provider/InfoProvider';
 import {AuthProvider} from 'USC_app_v1/Provider/AuthProvider';
 import Navigator from 'USC_app_v1/Routes/appStack';
 
-/*
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
-*/
+
 export default function App() {
   return (
     <AuthProvider>
