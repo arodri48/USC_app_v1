c1d108b0c799025ae4e51b0504f321f32cb338de
Alex <arodtony89@gmail.com>
2020-08-15 21:56:23 -0400

    latest version of app

--- Provider/AuthProvider.js
+++ Provider/AuthProvider.js
@@ -1,71 +1,76 @@
-import React, {useState, createContext, useReducer, useEffect, useMemo} from 'react';
+import React, {
+  useState,
+  createContext,
+  useReducer,
+  useEffect,
+  useMemo,
+} from 'react';
+import * as AsyncStorage from 'react-native/Libraries/Storage/AsyncStorage';
 
 //Create Context Object
 export const AuthContext = createContext();
 
-export const AuthProvider = props => {
-    const [state, dispatch] = useReducer(
-        (prevState, action) => {
-            switch(action.type){
-                case 'RESTORE_TOKEN':
-                    return {
-                        ...prevState,
-                        userToken: action.token,
-                        isLoading: false,
-                    };
-                case 'SIGN_IN':
-                    return{
-                        ...prevState,
-                        isSignout: false,
-                        userToken: action.token,
-                    };
-                case 'SIGN_OUT':
-                    return{
-                        ...prevState,
-                        isSignout: true,
-                        userToken: null,
-                    };
-            }
-        },
-        {
-            isLoading: true,
+export const AuthProvider = (props) => {
+  const [state, dispatch] = useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
             isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
             userToken: null,
-        }
-    );
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    },
+  );
 
-    useEffect(() => {
-        const bootstrapAsync = async () => {
-            let userToken;
+  useEffect(() => {
+    const bootstrapAsync = async () => {
+      let userToken;
 
-            try{
-                userToken = await AsyncStorage.getItem('userToken');
-            }
-            catch (e) {
-                // restoring token failed
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch (e) {
+        // restoring token failed
+      }
+      dispatch({type: 'RESTORE_TOKEN', token: userToken});
+    };
+    bootstrapAsync();
+  }, []);
 
-            }
-            dispatch({ type: 'RESTORE_TOKEN', token: userToken});
-        };
-        bootstrapAsync();
-    }, []);
+  const authContext = useMemo(
+    () => ({
+      signIn: () => {
+        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
+      },
+      signOut: () => dispatch({type: 'SIGN_OUT'}),
+      signUp: () => {
+        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
+      },
+    }),
+    [],
+  );
 
-    const authContext = useMemo(
-        () => ({
-            signIn: () => {
-                dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
-            },
-            signOut: () => dispatch({type: 'SIGN_OUT'}),
-            signUp: () => {
-                dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
-            },
-        }),
-        []
-    );
-
-    return(
-        <AuthContext.Provider value={{state, authContext}}>
-            {props.children}
-        </AuthContext.Provider>
-    );
-}
+  return (
+    <AuthContext.Provider value={{state, authContext}}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
