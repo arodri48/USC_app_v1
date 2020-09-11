acaf057fcdfd8bf45440c9bea0be139ba966f55f
Alex <arodtony89@gmail.com>
2020-09-11 13:19:44 -0400

    latest searchscreen

--- Screens/SearchScreen/index.js
+++ Screens/SearchScreen/index.js
@@ -16,15 +16,15 @@ import {
 } from 'react-native';
 import {InfoContext} from '../../Provider/InfoProvider';
 import {API} from 'aws-amplify';
-import { storesByCause} from '../../src/graphql/queries';
+import {storesByCause} from '../../src/graphql/queries';
 import FastImage from 'react-native-fast-image';
 import {Button, OverLay, CheckBox} from 'react-native-elements';
 import ImageList from 'USC_app_v1/media/ImageStore';
-import { List } from 'react-native-paper';
+import {List} from 'react-native-paper';
 
 const {width, height} = Dimensions.get('window');
 
-export default function SearchScreen({navigation}){
+export default function SearchScreen({navigation}) {
   // Hooks for store values
   const [
     setStoreName,
@@ -35,7 +35,7 @@ export default function SearchScreen({navigation}){
     setWebsite,
     setGoodsType,
     setImage,
-      setCause
+    setCause,
   ] = useContext(InfoContext);
   // Hooks for filter values
   const [filterVisible, setFilterVisible] = useState(false);
@@ -43,85 +43,99 @@ export default function SearchScreen({navigation}){
   const causeRef = useRef('');
   const toggleFilterOverlay = () => {
     setFilterVisible(!filterVisible);
-    if ((causeRef.current !== currentCause) && !filterVisible) {
+    if (causeRef.current !== currentCause && !filterVisible) {
       causeRef.current = currentCause;
       setRefresh(true);
     }
   };
   const [currentCause, setCurrentCause] = useState('');
-// Hooks for filter options
+  // Hooks for filter options
   const [filterOption, filterDispatch] = useReducer(
-      (prevState, action) => {
-        switch (action.type) {
-          case 'BLM':
-            if (filterOption.BLM){
-              setCurrentCause('');
-              return {
-                BLM: false
-              }
-            }
-            else {
-              setCurrentCause("BLM");
-              return {
-                BLM: true,
-              }
-            }
-        }
-      },
-      {
-        BLM: false
-      },
-  );    // Hooks for sort values
+    (prevState, action) => {
+      switch (action.type) {
+        case 'BLM':
+          if (filterOption.BLM) {
+            setCurrentCause('');
+            return {
+              ...prevState,
+              BLM: false,
+            };
+          } else {
+            setCurrentCause('BLM');
+            return {
+              mental_health: false,
+              BLM: true,
+            };
+          }
+        case 'Mental Health':
+          if (filterOption.mental_health) {
+            setCurrentCause('');
+            return {
+              ...prevState,
+              mental_health: false,
+            };
+          } else {
+            setCurrentCause('Mental Health');
+            return {
+              mental_health: true,
+              BLM: false,
+            };
+          }
+      }
+    },
+    {
+      BLM: false,
+      mental_health: false,
+    },
+  ); // Hooks for sort values
   const [sortVisible, setSortVisible] = useState(false);
   //const [sortAltered, setSortAltered] = useState(false);
   const sortRef = useRef('ASC');
   const toggleSortOverlay = () => {
     setSortVisible(!sortVisible);
-    if ((sortRef.current !== currentSort) && !sortVisible ) {
+    if (sortRef.current !== currentSort && !sortVisible) {
       sortRef.current = currentSort;
       setRefresh(true);
     }
   };
   const [currentSort, setCurrentSort] = useState('ASC');
   const [sortOption, sortDispatch] = useReducer(
-      (prevState, action) => {
-        switch (action.type) {
-          case 'HIGH_TO_LOW':
-            if (sortOption.high_to_low){
-              setCurrentSort('ASC');
-              return {
-                high_to_low: false,
-                low_to_high: true,
-              };
-            }
-            else{
-              setCurrentSort('DESC');
-              return {
-                high_to_low: true,
-                low_to_high: false,
-              };
-            }
-          case 'LOW_T0_HIGH':
-            if (sortOption.low_to_high){
-              setCurrentSort('DESC');
-              return {
-                high_to_low: true,
-                low_to_high: false,
-              };
-            }
-            else{
-              setCurrentSort('ASC');
-              return {
-                high_to_low: false,
-                low_to_high: true,
-              };
-            }
-        }
-      },
-      {
-        high_to_low: false,
-        low_to_high: true,
-      },
+    (prevState, action) => {
+      switch (action.type) {
+        case 'HIGH_TO_LOW':
+          if (sortOption.high_to_low) {
+            setCurrentSort('ASC');
+            return {
+              high_to_low: false,
+              low_to_high: true,
+            };
+          } else {
+            setCurrentSort('DESC');
+            return {
+              high_to_low: true,
+              low_to_high: false,
+            };
+          }
+        case 'LOW_T0_HIGH':
+          if (sortOption.low_to_high) {
+            setCurrentSort('DESC');
+            return {
+              high_to_low: true,
+              low_to_high: false,
+            };
+          } else {
+            setCurrentSort('ASC');
+            return {
+              high_to_low: false,
+              low_to_high: true,
+            };
+          }
+      }
+    },
+    {
+      high_to_low: false,
+      low_to_high: true,
+    },
   );
 
   // Hooks for managing store list
@@ -131,7 +145,7 @@ export default function SearchScreen({navigation}){
   const pageTokenRef = useRef();
   const storesRef = useRef([]);
 
-//Initial useEffect to load in data
+  //Initial useEffect to load in data
   useEffect(() => {
     async function fetchStores() {
       try {
@@ -144,7 +158,6 @@ export default function SearchScreen({navigation}){
           },
         });
         pageTokenRef.current = storeData.data.storesByCause.nextToken;
-
 
         storesRef.current = storeData.data.storesByCause.items;
         setLoading(false);
@@ -167,7 +180,6 @@ export default function SearchScreen({navigation}){
           },
         });
         pageTokenRef.current = storeData.data.storesByCause.nextToken;
-
 
         storesRef.current = storeData.data.storesByCause.items;
         setLoading(false);
@@ -206,7 +218,7 @@ export default function SearchScreen({navigation}){
       fetchStores();
       setLoadingMore(false);
     }
-  }, [loadingMore, pageTokenRef, storesRef]);    // Functions called upon by render
+  }, [loadingMore, pageTokenRef, storesRef]); // Functions called upon by render
   const _renderStore = ({store}) => {
     const storePressHandler = () => {
       // set InfoProvider variables to equal the store's attributes
@@ -223,24 +235,24 @@ export default function SearchScreen({navigation}){
       navigation.navigate('StoreScreen');
     };
     return (
-        <TouchableOpacity
-            style={{backgroundColor: 'pink'}}
-            onPress={storePressHandler}>
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Text>{store.storeName}</Text>
-            <Text>{store.PricePoint}</Text>
-          </View>
-          <View style={{flexDirection: 'row'}}>
-            <FastImage
-                style={{width: 100, height: 100}}
-                source={{
-                  uri: store.image,
-                }}
-                resizeMode={FastImage.resizeMode.contain}
-            />
-            <Text>store.cause</Text>
-          </View>
-        </TouchableOpacity>
+      <TouchableOpacity
+        style={{backgroundColor: 'pink'}}
+        onPress={storePressHandler}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Text>{store.storeName}</Text>
+          <Text>{store.PricePoint}</Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <FastImage
+            style={{width: 100, height: 100}}
+            source={{
+              uri: store.image,
+            }}
+            resizeMode={FastImage.resizeMode.contain}
+          />
+          <Text>store.cause</Text>
+        </View>
+      </TouchableOpacity>
     );
   };
 
@@ -251,19 +263,19 @@ export default function SearchScreen({navigation}){
       return null;
     }
     return (
-        <View
-            style={{
-              position: 'relative',
-              width: width,
-              height: height,
-              paddingVertical: 20,
-              borderTopWidth: 1,
-              marginTop: 10,
-              marginBottom: 10,
-              borderColor: 'pink',
-            }}>
-          <ActivityIndicator animating size="large" />
-        </View>
+      <View
+        style={{
+          position: 'relative',
+          width: width,
+          height: height,
+          paddingVertical: 20,
+          borderTopWidth: 1,
+          marginTop: 10,
+          marginBottom: 10,
+          borderColor: 'pink',
+        }}>
+        <ActivityIndicator animating size="large" />
+      </View>
     );
   };
 
@@ -271,72 +283,81 @@ export default function SearchScreen({navigation}){
     setLoadingMore(true);
   };
 
-
   const [causeFilterExpanded, setCauseFilterExpanded] = useState(true);
-  const handleCauseFilterPress = () => setCauseFilterExpanded(!causeFilterExpanded);
+  const handleCauseFilterPress = () =>
+    setCauseFilterExpanded(!causeFilterExpanded);
 
   // Render return
   return !loading ? (
-      <View style={styles.MainContainer}>
-        <Text style={styles.sendASmile}>Send A Smile</Text>
-        <Text style={styles.text}>A UniSelfCare Program</Text>
-        <FastImage source={ImageList.Smile} />
-        <View style={styles.FilterSortMenuContainer}>
-          <Button title="Filter" onPress={toggleFilterOverlay} />
+    <View style={styles.MainContainer}>
+      <Text style={styles.sendASmile}>Send A Smile</Text>
+      <Text style={styles.text}>A UniSelfCare Program</Text>
+      <FastImage source={ImageList.Smile} />
+      <View style={styles.FilterSortMenuContainer}>
+        <Button title="Filter" onPress={toggleFilterOverlay} />
 
-          <Button title="Sort" onPress={toggleSortOverlay} />
-        </View>
-        <OverLay
-            isVisible={filterVisible}
-            fullscreen={false}
-            onBackdropPress={toggleFilterOverlay}
-            overlayStyle={styles.filterMenu}>
-          <List.Accordion
-            title="Filter By Cause"
-            expanded={causeFilterExpanded}
-            onPress={handleCauseFilterPress}
-            >
-            <CheckBox
-              title='BLM'
-              checkedIcon='dot-circle-o'
-              uncheckedIcon='circle-o'
-              checked={filterOption.BLM}
-              onPress={() => filterDispatch('BLM')}
-            />
-          </List.Accordion>
-        </OverLay>
-        <OverLay
-            isVisible={sortVisible}
-            fullscreen={false}
-            onBackdropPress={toggleSortOverlay}
-            overlayStyle={styles.sortMenu}>
-          <Button
-              title="Prices High to Low"
-              containterStyle={{backgroundColor: sortOption.high_to_low ? 'green' : 'transparent'}}
-              onPress={() => sortDispatch('HIGH_TO_LOW')}
+        <Button title="Sort" onPress={toggleSortOverlay} />
+      </View>
+      <OverLay
+        isVisible={filterVisible}
+        fullscreen={false}
+        onBackdropPress={toggleFilterOverlay}
+        overlayStyle={styles.filterMenu}>
+        <List.Accordion
+          title="Filter By Cause"
+          expanded={causeFilterExpanded}
+          onPress={handleCauseFilterPress}>
+          <CheckBox
+            title="BLM"
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            checked={filterOption.BLM}
+            onPress={() => filterDispatch('BLM')}
           />
-          <Button
-              title='Prices Low to High'
-              containterStyle={{backgroundColor: sortOption.low_to_high ? 'green' : 'transparent'}}
-              onPress={() => sortDispatch('LOW_TO_HIGH')}
+          <CheckBox
+            title="Mental Health"
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            checked={filterOption.mental_health}
+            onPress={() => filterDispatch('Mental Health')}
           />
-        </OverLay>
-        <FlatList
-            data={storesRef.current}
-            renderItem={_renderStore}
-            onEndReached={_handleLoadMore}
-            onEndReachedThreshold={0.5}
-            keyExtractor={_keyExtractor}
-            initialNumToRender={10}
-            ListFooterComponent={_renderFooter}
+        </List.Accordion>
+      </OverLay>
+      <OverLay
+        isVisible={sortVisible}
+        fullscreen={false}
+        onBackdropPress={toggleSortOverlay}
+        overlayStyle={styles.sortMenu}>
+        <Button
+          title="Prices High to Low"
+          containterStyle={{
+            backgroundColor: sortOption.high_to_low ? 'green' : 'transparent',
+          }}
+          onPress={() => sortDispatch('HIGH_TO_LOW')}
         />
-      </View>
-
+        <Button
+          title="Prices Low to High"
+          containterStyle={{
+            backgroundColor: sortOption.low_to_high ? 'green' : 'transparent',
+          }}
+          onPress={() => sortDispatch('LOW_TO_HIGH')}
+        />
+      </OverLay>
+      <FlatList
+        data={storesRef.current}
+        renderItem={_renderStore}
+        onEndReached={_handleLoadMore}
+        onEndReachedThreshold={0.5}
+        keyExtractor={_keyExtractor}
+        initialNumToRender={10}
+        ListFooterComponent={_renderFooter}
+      />
+    </View>
   ) : (
-      <View>
-        <Text style={{alignSelf: 'center'}}>Loading Stores</Text>
-        <ActivityIndicator />
-      </View>
+    <View>
+      <Text style={{alignSelf: 'center'}}>Loading Stores</Text>
+      <ActivityIndicator />
+    </View>
   );
 }
 const styles = StyleSheet.create({
@@ -370,6 +391,5 @@ const styles = StyleSheet.create({
     paddingTop: 10,
   },
 });
-
 
 // TODO:  create objects , then test and restyle
