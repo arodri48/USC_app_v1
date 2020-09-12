2424a18741d0c1b331c217b29a05d1b879054c65
Alex <arodtony89@gmail.com>
2020-09-11 23:33:10 -0400

    finished the top header design, now need to fix the issue with reducer

--- Screens/SearchScreen/index.js
+++ Screens/SearchScreen/index.js
@@ -18,10 +18,8 @@ import {InfoContext} from '../../Provider/InfoProvider';
 import {API, graphqlOperation} from 'aws-amplify';
 import {listStores, storesByPrice} from '../../src/graphql/queries';
 import FastImage from 'react-native-fast-image';
-import {Button, OverLay, CheckBox} from 'react-native-elements';
+import {Button, Overlay, CheckBox} from 'react-native-elements';
 import ImageList from 'USC_app_v1/media/ImageStore';
-import {List} from 'react-native-paper';
-import {color} from 'react-native-reanimated';
 
 const {width, height} = Dimensions.get('window');
 
@@ -64,6 +62,7 @@ export default function SearchScreen({navigation}) {
           } else {
             setCurrentCause('BLM');
             return {
+              ...prevState,
               mental_health: false,
               BLM: true,
             };
@@ -78,6 +77,7 @@ export default function SearchScreen({navigation}) {
           } else {
             setCurrentCause('Mental Health');
             return {
+              ...prevState,
               mental_health: true,
               BLM: false,
             };
@@ -107,12 +107,14 @@ export default function SearchScreen({navigation}) {
           if (sortOption.high_to_low) {
             setCurrentSort('ASC');
             return {
+              ...prevState,
               high_to_low: false,
               low_to_high: true,
             };
           } else {
             setCurrentSort('DESC');
             return {
+              ...prevState,
               high_to_low: true,
               low_to_high: false,
             };
@@ -121,12 +123,14 @@ export default function SearchScreen({navigation}) {
           if (sortOption.low_to_high) {
             setCurrentSort('DESC');
             return {
+              ...prevState,
               high_to_low: true,
               low_to_high: false,
             };
           } else {
             setCurrentSort('ASC');
             return {
+              ...prevState,
               high_to_low: false,
               low_to_high: true,
             };
@@ -353,6 +357,28 @@ export default function SearchScreen({navigation}) {
 
         <Button title="Sort" onPress={toggleSortOverlay} buttonStyle={{backgroundColor : 'transparent'}} titleStyle={{color : 'black'}}/>
       </View>
+      <Overlay
+          onBackdropPress={toggleFilterOverlay}
+          overlayStyle={styles.filterMenu}
+          isVisible={filterVisible}
+      >
+        <View>
+          <CheckBox
+              title="BLM"
+              checkedIcon="dot-circle-o"
+              uncheckedIcon="circle-o"
+              checked={filterOption.BLM}
+              onPress={() => filterDispatch('BLM')}
+          />
+          <CheckBox
+              title="Mental Health"
+              checkedIcon="dot-circle-o"
+              uncheckedIcon="circle-o"
+              checked={filterOption.mental_health}
+              onPress={() => filterDispatch('Mental Health')}
+          />
+        </View>
+      </Overlay>
 
 
     </View>
@@ -377,7 +403,6 @@ const styles = StyleSheet.create({
     marginTop: 18,
     justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: 'red'
   },
   filterMenu: {
     height: 400,
