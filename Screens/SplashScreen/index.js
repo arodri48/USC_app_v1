63cc6e1f7a6d68a9d53612e984cd58a5fcbf06db
MRSA-J <mrsa@brandeis.edu>
2020-09-15 15:30:25 +0800

    some update, not finished yet

--- Screens/SplashScreen/index.js
+++ Screens/SplashScreen/index.js
@@ -1,10 +1,44 @@
 import React, {useContext} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+
+import FastImage from 'react-native-fast-image';
+import ImageList from '../../media/ImageStore';
+
+
+const {width, height} = Dimensions.get('window');
 
 export default function SplashScreen({navigation}){
   return(
-      <View>
+      <View style={styles.MainContainer}>
           <Text>Loading...</Text>
+          <View style={styles.image}>
+            <FastImage
+              style={{width: 300, height: 300}}
+              source={ImageList.Hands}
+              resizeMode={FastImage.resizeMode.contain}
+            />
+          </View>
       </View>
   )
 }
+
+const styles = StyleSheet.create({
+
+    MainContainer: {
+        position:'absolute',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        width: width,
+        height: height,
+    },
+
+    image:{
+      alignItems:'center',
+      justifyContent:'center',
+      width:200,
+      height:200,
+    }
+
+
+});
